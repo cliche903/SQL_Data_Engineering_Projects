edc6da631a339f117a46834e4a994fd258eb1232
Claire Lansdale <cliche903@gmail.com>
2026-05-28 14:59:09 -0700

    Format SQL query for better readability

--- 1_EDA/02_top_paying_skills.sql
+++ 1_EDA/02_top_paying_skills.sql
@@ -12,9 +12,10 @@ SELECT
     sd.skills,
     ROUND(MEDIAN(jpf.salary_year_avg), 0) AS median_salary,
     COUNT(jpf.*) AS demand_count
-FROM job_postings_fact AS jpf
-    JOIN skills_job_dim AS sjd ON jpf.job_id = sjd.job_id
-    JOIN skills_dim AS sd ON sjd.skill_id = sd.skill_id
+FROM 
+    job_postings_fact AS jpf
+JOIN skills_job_dim AS sjd ON jpf.job_id = sjd.job_id
+JOIN skills_dim AS sd ON sjd.skill_id = sd.skill_id
 WHERE 
     jpf.job_title_short = 'Data Engineer'
     AND jpf.job_work_from_home = TRUE
