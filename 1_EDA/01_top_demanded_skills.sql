e7ad16f95215e10fa79f13cb5b95108ba2e7d8b0
Claire Lansdale <cliche903@gmail.com>
2026-05-28 14:58:40 -0700

    Format SQL query for better readability

--- 1_EDA/01_top_demanded_skills.sql
+++ 1_EDA/01_top_demanded_skills.sql
@@ -10,9 +10,10 @@
 SELECT 
     sd.skills,
     COUNT(jpf.*) AS demand_count
-FROM job_postings_fact AS jpf
-    JOIN skills_job_dim AS sjd ON jpf.job_id = sjd.job_id
-    JOIN skills_dim AS sd ON sjd.skill_id = sd.skill_id
+FROM 
+  job_postings_fact AS jpf
+JOIN skills_job_dim AS sjd ON jpf.job_id = sjd.job_id
+JOIN skills_dim AS sd ON sjd.skill_id = sd.skill_id
 WHERE 
     jpf.job_title_short = 'Data Engineer'
     AND jpf.job_work_from_home = TRUE
