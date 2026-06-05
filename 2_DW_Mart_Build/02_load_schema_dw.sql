3fb4ceafd7fe8e3c64bf6175d94a71e1e135013d
Claire Lansdale <cliche903@gmail.com>
2026-06-05 09:27:01 -0700

    data validation check sat

--- 2_DW_Mart_Build/02_load_schema_dw.sql
+++ 2_DW_Mart_Build/02_load_schema_dw.sql
@@ -40,3 +40,23 @@ SELECT skill_id, job_id
 FROM 
     read_csv('https://storage.googleapis.com/sql_de/skills_job_dim.csv',
     AUTO_DETECT = TRUE);
+
+SELECT 'company_dim' AS table_name, COUNT(*) AS record_count FROM company_dim
+UNION ALL
+SELECT 'skills_dim', COUNT(*) FROM skills_dim
+UNION ALL
+SELECT 'job_postings_fact', COUNT(*) FROM job_postings_fact
+UNION ALL
+SELECT 'skills_job_dim', COUNT(*) FROM skills_job_dim;
+
+SELECT '=== Company Dimension Sample ===' AS info;
+SELECT * FROM company_dim LIMIT 5;
+
+SELECT '=== Skill Dimension Sample ===' AS info;
+SELECT * FROM skills_dim LIMIT 5;
+
+SELECT '=== Job Postings Fact Sample ===' AS info;
+SELECT * FROM job_postings_fact LIMIT 5;
+
+SELECT '=== Skills Job Bridge Sample ===' AS info;
+SELECT * FROM skills_job_dim LIMIT 5;
